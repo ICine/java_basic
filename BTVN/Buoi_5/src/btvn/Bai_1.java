32dc649d97123f65df89fd75b40ee5968fba4075
Hoang Manh Hung <hmh290204@gmail.com>
2023-08-25 21:23:14 +0700

    25/08

---
+++ BTVN/Buoi_5/src/btvn/Bai_1.java
@@ -0,0 +1,15 @@
+package btvn;
+
+import entities.Hang;
+
+public class Bai_1 {
+    public static void main(String[] args) {
+        Hang hang = new Hang();
+
+        hang.Nhap();
+        
+        System.out.println("\n");
+        hang.Xuat();
+        System.out.println("\n");
+    }
+}
---
+++ BTVN/Buoi_5/src/entities/Hang.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Hang {
+    private String MaHang;
+    private String TenHang;
+    private NSX x = new NSX();
+
+    public String getMaHang() {
+        return MaHang;
+    }
+
+    public void setMaHang(String maHang) {
+        MaHang = maHang;
+    }
+
+    public String getTenHang() {
+        return TenHang;
+    }
+
+    public void setTenHang(String tenHang) {
+        TenHang = tenHang;
+    }
+
+    public NSX getX() {
+        return x;
+    }
+
+    public void setX(NSX x) {
+        this.x = x;
+    }
+
+    public Hang(String maHang, String tenHang, NSX x) {
+        MaHang = maHang;
+        TenHang = tenHang;
+        this.x = x;
+    }
+
+    public Hang() {
+    }
+
+    public void Nhap(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Nhap ma hang: ");
+        MaHang = sc.nextLine();
+        System.out.print("Nhap ten hang: ");
+        TenHang = sc.nextLine();
+        x.Nhap();
+    }
+    
+
+    public void Xuat(){
+        System.out.println("MaHang= " + MaHang + ", TenHang= " + TenHang);
+        x.Xuat();
+    }
+
+
+
+}
