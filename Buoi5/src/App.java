185ed041d2af4a9b06c1fa359482064caed4b6b0
Hung Hoang Manh <130200904+ICine@users.noreply.github.com>
2023-08-25 22:50:25 +0700

    Delete Buoi5 directory

--- Buoi5/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Line l = new Line(1, 1, 2, 2);
-        System.out.println(l.getLength());
-
-    }
-}
--- Buoi5/src/hello/world.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class world {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
