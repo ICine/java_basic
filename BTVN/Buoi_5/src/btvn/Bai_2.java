ac6334a7bed4ffc869aa1ef38a8f8fbc43846ee5
Hoang Manh Hung <hmh290204@gmail.com>
2023-08-25 22:49:24 +0700

    25/08/2023

---
+++ BTVN/Buoi_5/src/btvn/Bai_2.java
@@ -0,0 +1,33 @@
+package btvn;
+
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Bai_2 {
+    public static void main(String[] args) {
+        int n;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhap so luong hoc sinh: ");
+            n = sc.nextInt();
+            if (n <= 0)
+                System.out.println("Invalid input");
+            else
+                break;
+        }
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student();
+            students[i].input();
+        }
+
+        System.out.println("\n\n");
+        for (Student i : students) {
+            i.output();
+        }
+        System.out.println("\n\n");
+
+    }
+}
---
+++ BTVN/Buoi_5/src/entities/Faculty.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Faculty {
+    private String Name;
+    private String Date;
+    private School x = new School();
+
+    public Faculty(){
+
+    }
+    public Faculty(String name, String date, School x) {
+        Name = name;
+        Date = date;
+        this.x = x;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getDate() {
+        return Date;
+    }
+    public void setDate(String date) {
+        Date = date;
+    }
+    public School getX() {
+        return x;
+    }
+    public void setX(School x) {
+        this.x = x;
+    }
+    
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Nhap ten khoa: ");
+        Name = sc.nextLine();
+        System.out.print("Nhap ngay: ");
+        Date = sc.nextLine();
+    }
+
+    public void output(){
+        System.out.printf("%-20s%-10s%n", Name, Date);
+        x.output();
+
+    }
+}
